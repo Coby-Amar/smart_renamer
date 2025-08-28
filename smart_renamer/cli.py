e0fa38a19f94c49d14b8f630b5cc9c8a552b633a
bladexbreaker <bladexbreaker@example.com>
2025-08-29 01:11:04 +0300

    Switch project license to MIT

--- smart_renamer/cli.py
+++ smart_renamer/cli.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2025 Coby Amar
+
 import sys
 
 from smart_renamer.renamer import main
--- smart_renamer/gui.py
+++ smart_renamer/gui.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2025 Coby Amar
+
 import tkinter as tk
 from pathlib import Path
 from tkinter import filedialog, messagebox, scrolledtext
